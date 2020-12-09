cee6bd79bcb62cc17e042c72d2f7d96199ae00ed
GC <52378162+gc394@users.noreply.github.com>
2020-12-09 22:22:16 +0000

    A start

--- DA.py
+++ DA.py
@@ -26,7 +26,7 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
-driver = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
+driver = webdriver.Chrome('chromedriver', chrome_options = chrome_options)
 
 # Pulling most recent BB entry
 
