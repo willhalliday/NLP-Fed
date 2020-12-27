b81d24a4c01dd6fe1ec11086044da38144905a25
GC <52378162+gc394@users.noreply.github.com>
2020-12-27 19:11:04 +0000

    Pulls and cleans all Beige Book Extracts from 2017

--- DA.py
+++ DA.py
@@ -1,4 +1,3 @@
-
 # https://code.visualstudio.com/docs/python
 # Modules
 
@@ -20,12 +19,6 @@ import random as rd
 BB_2020_url = 'https://www.federalreserve.gov/monetarypolicy/beige-book-default.htm'
 BB_2019_1996_urls = 'https://www.federalreserve.gov/monetarypolicy/beige-book-archive.htm' # 2017-2020 has the same format
 
-# Creating the dataframe
-
-date = []
-overallEconomicActivity = []
-employmentPrices = []
-
 # Configure Chrome Options for webdriver
 
 chrome_options = Options()
@@ -36,26 +29,64 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome('/Users/Cookie/node_modules/chromedriver/lib/chromedriver/chromedriver', 
                           chrome_options = chrome_options)
 
-# Pulling most recent BB entry
+# Creates links list
 
-print('Pulling ' + BB_2020_url)
+BB_urls = []    
 
-driver.get(BB_2020_url)
+for yr in range(1996,2020):
+    
+        BB_urls.append('https://www.federalreserve.gov/monetarypolicy/beigebook' + str(yr) + '.htm')
+        
+BB_urls.append(BB_2020_url)   
 
-print('Loading page')
+links = []
 
-tm.sleep(rd.randint(2, 4))
+for BB_url in BB_urls:
 
-soup = BeautifulSoup(driver.page_source, 'lxml')
+    print('Pulling ' + str(BB_url))
 
-links = [str(l['href']) for l in soup.find_all("a", href=re.compile("/monetarypolicy/beigebook"))]
+    driver.get(BB_url)
 
-links = ['https://www.federalreserve.gov' + sub for sub in links]
+    print('Loading page')
+
+    tm.sleep(rd.randint(2, 4))
+
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+
+    foundlinks = [str(l['href']) for l in soup.find_all("a", href=re.compile(r"[/]monetarypolicy[/]beigebook.*.htm"))]
+
+    links.append(foundlinks)    
+
+links = [item for sublist in links for item in sublist]
+
+updated_links = []
+
+for link in links:
+    
+    if 'https://www.federalreserve.gov' not in link:
+        
+        updated_links.append('https://www.federalreserve.gov' + link)
+    
+    else:
+    
+        updated_links.append(link)
 
 print('Collected Links')
 
-for n, link in enumerate(links):
+def simpleCleanCorpus(corpus: str): 
 
+            corpus = re.sub('\n|<p>|</p>|<br/>|<strong>.*</strong>', '', corpus)
+
+            return corpus
+        
+# Creating the dataframe
+
+date = []
+overallEconomicActivity = []
+employmentPrices = []
+        
+for n, link in enumerate(updated_links):
+    
     driver.get(link)
 
     print('Loading link ' + str(n))
@@ -64,35 +95,37 @@ for n, link in enumerate(links):
 
     reportSoup = BeautifulSoup(driver.page_source, 'lxml')
 
-    date.append(re.sub('Last Update:|\n|\\s{2,}',
-                       '',
-                       reportSoup.find('div', {'class':'lastUpdate'}).text)) # Pulling date for the dataframe
-
-    tm.sleep(rd.randint(1, 3))
-
-    textSoup = [str(t) for t in reportSoup.find_all('p')]
-
-    positionOEA = [i for i, s in enumerate(textSoup) if 'Overall Economic Activity' in s][0]
-    positionEP = [i for i, s in enumerate(textSoup) if 'Employment and Wages' in s][0]
-    positionEnd = [i for i, s in enumerate(textSoup) if 'Highlights by Federal Reserve District' in s][0]
-
-    corpusOverallEconomicActivity = ''.join(textSoup[positionOEA:positionEP])
-    corpusEmploymentPrices = ''.join(textSoup[positionEP:positionEnd])
-
-    def simpleCleanCorpus(corpus: str): 
-
-        corpus = re.sub('<p>|</p>|<br/>|<strong>.*</strong>', '', corpus)
-
-        return corpus
-
-    overallEconomicActivity.append(simpleCleanCorpus(corpusOverallEconomicActivity))
-
-    employmentPrices.append(simpleCleanCorpus(corpusEmploymentPrices))
-
-    print('Corpuses from link ' + str(n) + 'cleaned and collected')
+    if any(ext in link for ext in ['2017', '2018', '2019','2020']):
     
-beigeBookExtracts2020 = pd.DataFrame({"Date":date,
-                                      "OverallEconomicActivity":overallEconomicActivity,
-                                      "EmploymentPrices":employmentPrices})
+        date.append(re.sub('Last Update:|\n|\\s{2,}',
+                           '',
+                           reportSoup.find('div', {'class':'lastUpdate'}).text)) # Pulling date for the dataframe
 
-print(beigeBookExtracts2020)
+        textSoup = [str(t) for t in reportSoup.find_all('p')]
+
+        positionOEA = [i for i, s in enumerate(textSoup) if 'Overall Economic Activity' in s][0]
+        positionEW = [i for i, s in enumerate(textSoup) if 'Employment and Wages' in s][0]
+        positionP = [i for i, s in enumerate(textSoup) if 'Prices' in s][0]
+
+        corpusOverallEconomicActivity = ''.join(textSoup[positionOEA:positionEW])
+        corpusEmploymentPrices = ''.join(textSoup[positionEW:(positionP+1)])   
+
+        overallEconomicActivity.append(simpleCleanCorpus(corpusOverallEconomicActivity))
+
+        employmentPrices.append(simpleCleanCorpus(corpusEmploymentPrices))
+
+        print('Corpuses from link ' + str(n) + ' cleaned and collected')
+        
+#     else:
+        
+#         date.append(re.sub('Last update:|\n|\\s{2,}',
+#                            '',
+#                            soup.find('div', {'id':'lastUpdate'}).text))
+        
+        
+        
+beigeBookExtracts = pd.DataFrame({"Date":date,
+                                  "OverallEconomicActivity":overallEconomicActivity,
+                                  "EmploymentPrices":employmentPrices})
+
+print(beigeBookExtracts)
